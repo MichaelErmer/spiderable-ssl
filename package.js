bd02bcfa627363a7079f4b2bfdd59d01c887963b
Julien <julien.le.coupanec@gmail.com>
2014-10-07 23:06:07 +0200

    spiderable package with --ignore-ssl-errors

---
+++ package.js
@@ -0,0 +1,26 @@
+Package.describe({
+  summary: "Meteor Spiderable Package with --ignore-ssl-errors",
+  version: "0.0.2"
+});
+
+Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0")
+
+  api.use('webapp', 'server');
+  api.use(['templating'], 'client');
+  api.use(['underscore'], ['client', 'server']);
+
+  api.export('Spiderable');
+
+  api.add_files('spiderable.html', 'client');
+  api.add_files('spiderable.js', ['client', 'server']);
+  api.add_files('spiderable_server.js', 'server');
+  api.add_files('spiderable_client.js', 'client');
+
+  api.add_files('phantom_script.js', 'server', { isAsset: true });
+});
+
+Package.on_test(function (api) {
+  api.use(['spiderable', 'tinytest']);
+  api.add_files('spiderable_tests.js', 'server');
+});
