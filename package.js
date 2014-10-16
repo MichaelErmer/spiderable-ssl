bc84536628406db6b5e7b2c47993b5f7424bbef4
Julien <julien.le.coupanec@gmail.com>
2014-10-17 00:46:33 +0200

    Spiderable SSL 0.0.4

--- package.js
+++ package.js
@@ -1,6 +1,7 @@
 Package.describe({
   summary: "Meteor Spiderable Package with --ignore-ssl-errors",
-  version: "0.0.2"
+  version: "0.0.4",
+  git: 'https://github.com/Gentlenode/spiderable-ssl'
 });
 
 Package.on_use(function (api) {
@@ -12,15 +13,6 @@ Package.on_use(function (api) {
 
   api.export('Spiderable');
 
-  api.add_files('spiderable.html', 'client');
   api.add_files('spiderable.js', ['client', 'server']);
   api.add_files('spiderable_server.js', 'server');
-  api.add_files('spiderable_client.js', 'client');
-
-  api.add_files('phantom_script.js', 'server', { isAsset: true });
-});
-
-Package.on_test(function (api) {
-  api.use(['spiderable', 'tinytest']);
-  api.add_files('spiderable_tests.js', 'server');
 });
